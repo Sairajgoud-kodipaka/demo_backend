648821d31bb61fd936f15ae1bd3595bcde37e3f8
Sairaj K <sairajgoudkodipaka@gmail.com>
2025-08-19 23:24:15 +0530

    commit-3

--- start.sh
+++ start.sh
@@ -1,1 +1,21 @@
- 
+#!/bin/bash
+
+# Exit on any error
+set -e
+
+# Run migrations
+echo "Running migrations..."
+python manage.py migrate
+
+# Collect static files
+echo "Collecting static files..."
+python manage.py collectstatic --noinput
+
+# Start Gunicorn
+echo "Starting Gunicorn..."
+exec gunicorn core.wsgi:application \
+    --bind 0.0.0.0:$PORT \
+    --workers 2 \
+    --timeout 120 \
+    --access-logfile - \
+    --error-logfile - 
