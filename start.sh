177296697d1d16bf33b70ad2dd61d74793b7b4eb
Sairaj K <sairajgoudkodipaka@gmail.com>
2025-08-19 23:01:59 +0530

    commit

--- start.sh
+++ start.sh
@@ -1,13 +1,1 @@
-#!/bin/bash
-
-# Set default port if not provided
-export PORT=${PORT:-8000}
-
-# Wait for database to be ready (optional)
-echo "Starting Django application on port $PORT..."
-
-# Run database migrations
-python manage.py migrate --noinput
-
-# Start the application
-python manage.py runserver 0.0.0.0:$PORT
+ 
